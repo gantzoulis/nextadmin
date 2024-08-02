8ca000c057df917b1fd77a942e975c188e9d0740
George Antzoulis <gantzoulis@hotmail.com>
2024-08-02 16:45:53 +0300

    Right Bar

--- app/ui/dashboard/rightbar/rightbar.jsx
+++ app/ui/dashboard/rightbar/rightbar.jsx
@@ -1,6 +1,34 @@
+import Image from "next/image";
+import styles from "./rightbar.module.css";
+import { MdPlayCircleFilled } from "react-icons/md";
+
+
 const Rightbar = () => {
   return (
-    <div>Rightbar</div>
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.bgContainer}> 
+          <Image src="/astronaut.png" alt="" fill className={styles.bg}/>
+        </div>
+        <div className={styles.texts}>
+          <span className={styles.notification}> Available Now </span>
+          <h3 className={styles.title}>How to use this version of the admin Dashboard</h3>
+          <span className={styles.subtitle}> Takes 4 minutes to learn</span>
+          <p className={styles.description}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Inventore iste commodi voluptates dignissimos quis voluptatibus, 
+            eum provident vel quisquam necessitatibus voluptatem modi, 
+            praesentium obcaecati quos adipisci minus id facilis recusandae.
+          </p>
+          <button className={styles.button}>
+            <MdPlayCircleFilled/>
+            Watch
+          </button>
+        </div>
+       
+      </div>
+
+    </div>
   )
 }
 
